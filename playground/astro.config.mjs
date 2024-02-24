1a9258463b1efddb757878385405bbab241685fd
Adam Matthiesen <amatthiesen@outlook.com>
2024-02-24 08:03:37 -0800

    some progress.  and prep to add new component

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import astroGist from "@matthiesenxyz/astro-gists";
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroGist()]
